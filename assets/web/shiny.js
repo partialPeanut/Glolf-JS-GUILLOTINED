3eb68de034334a953e31370ff4b8e43def5306c8
bubblegumpumkin <125165994+bubblegumpumpkin@users.noreply.github.com>
2023-06-22 03:22:58 -0500

    AJAX / XMLHttpRequest Test

--- assets/web/shiny.js
+++ assets/web/shiny.js
@@ -170,6 +170,17 @@ function updateInterval() {
 
 //===========================================================================================
 
+function loadDoc() {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            document.getElementById("content").innerHTML = this.responseText;
+           }
+    };
+    xhttp.open("GET", "/pages/ajax_info.txt", true);
+    xhttp.send(); 
+  }
+
 function switchFeedsToOne() {
     document.getElementById("feed-displayers").style.gridTemplateColumns = "1fr"
     document.getElementById("feed-displayers").style.gridTemplateRows = "1fr"
